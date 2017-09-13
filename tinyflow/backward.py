911df8987f0f1bed78f14fbe7b1fe325330610ef
Anvith Shivakumara <s.anvith@gmail.com>
2017-09-13 23:33:46 +0530

    Added methods for backward propogation + more helper methods

--- tinyflow/backward.py
+++ tinyflow/backward.py
@@ -1,10 +1,47 @@
+import numpy as np
+from activations import relu_backward, sigmoid_backward
+
+
 def linear_backward(dZ, cache):
-    pass
+	A_prev, W, b = cache
+	m = A_prev.shape[1]
+
+	dW = 1. / m * np.dot(dZ, np.transpose(A_prev))
+    db = 1./m * np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(np.transpose(W), dZ)
+
+    return dA_prev, dW, db
 
 
 def linear_activation_backward(dA, cache, activation):
-    pass
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+    	dZ = relu_backward(dA, activation_cache)
+    elif activation == "sigmoid":
+    	dZ = sigmoid_backward(dA, activation_cache)
+    dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
 
 
-def L_model_backward(AL, Y, cache):
-    pass
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches)
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+
+    dAL = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+    current_cache = caches[L-1]
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache, activation="sigmoid")
+
+
+    for l in reversed(range(L-1)):
+    	current_cache = caches[l]
+    	dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 2)], current_cache, activation="relu")
+    	grads["dA" + str(l + 1)] = dA_prev_temp
+    	grads["dW" + str(l + 1)] = dW_temp
+    	grads["db" + str(l + 1)] = db_temp
+
+    return grads
--- tinyflow/general.py
+++ tinyflow/general.py
@@ -1,22 +1,72 @@
+import numpy as np
+from forward import L_model_forward
+
+
 def load_data():
     pass
 
 
 def initialize_parameters(n_x, n_h, n_y):
-    pass
+    np.random.seed(1)
+
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+
+    return parameters
 
 
 def initialize_parameters_deep(layer_dims):
-    pass
+    np.random.seed(1)
+    parameters = {}
+    L = len(layer_dims)
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l],
+                                                   layer_dims[l - 1]) / np.sqrt(layer_dims[l - 1])
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+    return parameterss
 
 
 def compute_cost(AL, Y):
-    pass
+    m = Y.shape[1]
+
+    cost = (1. / m) * (-np.dot(Y, np.log(AL).T) -
+                       np.dot(1 - Y, np.log(1 - AL).T))
+    cost = np.squeeze(cost)
+
+    return cost
 
 
 def update_parameters(parameters, grads, learning_rate):
-    pass
+    L = len(parameters) // 2
+
+    for l in range(L):
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - \
+            learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - \
+            learning_rate * grads["db" + str(l + 1)]
+
+    return parameters
 
 
 def predict(X, y, parameters):
-    pass
+    m = X.shape[1]
+    n = len(parameters) // 2
+    p = np.zeros((1, m))
+
+    probas, caches = L_model_forward(X, parameters)
+
+    for i in range(0, probas.shape[1]):
+        if probas[0, i] > 0.5:
+            p[0, i] = 1
+        else:
+            p[0, i] = 0
+
+    print("Accuracy: " + str(np.sum((p == y) / m)))
+
+    return p
